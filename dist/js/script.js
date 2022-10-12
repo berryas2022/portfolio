359a8ce219754b0cacfabcbed7c1ac354221732b
BerryMHD <berry.as.2022@gmail.com>
2022-10-13 00:44:07 +0800

    dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,16 @@
 //navbar fixed
 window.onscroll= function () {
     const header = document.querySelector('header')
+    const btt = document.querySelector('#btt')
     const fixedNav = header.offsetTop
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed')
+        btt.classList.remove('hidden')
+        btt.classList.add('flex')
     }else{
         header.classList.remove('navbar-fixed')
+        btt.classList.remove('flex')
+        btt.classList.add('hidden')
     }
 }
 
@@ -16,3 +21,31 @@ burger.addEventListener('click', function() {
     burger.classList.toggle('burger-active')
     navMenu.classList.toggle('hidden')
 })
+
+//hidden nav toggle
+
+window.addEventListener('click', function(e) {
+    if (e.target != burger && e.target != navMenu) {
+        burger.classList.remove('burger-active')
+        navMenu.classList.add('hidden')
+    }
+})
+
+const darkToggle = document.querySelector("#dark-toggle")
+const html = document.querySelector('html')
+
+darkToggle.addEventListener('click', function(e) {
+    if (darkToggle.checked) {
+        html.classList.add('dark')
+        localStorage.theme = 'dark'
+    } else {
+        html.classList.remove('dark')
+        localStorage.theme = 'light'
+    }
+})
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true
+} else {
+    darkToggle.checked = false
+}
